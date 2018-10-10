3a85b8414977ff6af0faa2a0ecc59bae673d90a7
dirzyk <44027941+dirzyk@users.noreply.github.com>
2018-10-10 22:16:38 +0200

    add 1zad.java

---
+++ 1zad.java
@@ -0,0 +1,15 @@
+﻿//Dawid Irzyk 1K311/PO
+
+class Main {
+
+  public static void main(String[] args) {
+    int a = 1, b = 13;
+    if (fun2(a, b))
+      System.out.println("teen");
+  }
+
+  public static boolean fun2(int a, int b) {
+    return ((a >= 13 && a <= 19) && (b < 13 || b > 19)) || ((b >= 13 && b <= 19) && (a < 13 || a > 19));
+  }
+
+}
