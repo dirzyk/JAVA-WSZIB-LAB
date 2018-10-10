142a745cf4d5f8a086b8346677936790baae76c2
dirzyk <44027941+dirzyk@users.noreply.github.com>
2018-10-10 21:47:37 +0200

    add 2 zad.java

---
+++ 2zad.java
@@ -0,0 +1,20 @@
+﻿//Dawid Irzyk 1K311/PO
+
+class Main {
+
+  public static void main(String[] args) {
+    System.out.println(suma(1, 13, 3));
+  }
+
+  public static int suma(int a, int b, int c) {
+    if (a == 13)
+      return 0;
+    if (b == 13)
+      return a;
+    if (c == 13)
+      return a + b;
+
+    return a + b + c;
+  }
+
+}
