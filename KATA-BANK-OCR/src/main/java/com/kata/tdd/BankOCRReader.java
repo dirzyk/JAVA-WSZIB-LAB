7cad749d5d92d442edf64e9f9f19bda5fcdb4e37
dirzyk <44027941+dirzyk@users.noreply.github.com>
2018-11-14 23:23:10 +0100

    Delete BankOCRReader.java

--- KATA-BANK-OCR/src/main/java/com/kata/tdd/BankOCRReader.java
+++
@@ -1,9 +0,0 @@
-package com.kata.tdd;
-
-import java.io.IOException;
-import java.net.URISyntaxException;
-import java.util.List; 
-
-public interface BankOCRReader {
-    List<String> readLinesFrom(String source) throws IOException, URISyntaxException;
-}
